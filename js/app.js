2b03ce853ab11bbea14fcf395b541d10cffad63b
ciatph <30580083+ciatph@users.noreply.github.com>
2017-09-05 23:46:53 +0800

    Add files via upload
    - used color coding for VA_Class (high, very high, low, very low, moderate)
    - added code stubs for popup display on hover
    - added CSS for VA_Class legends; map overlay div
    - revised VA_Class color coding
    - added VA_Class legends
    - removed gray background for mobile media query

--- js/app.js
+++ js/app.js
@@ -2,11 +2,41 @@
 
 // container for provinces
 var provinces = [];
+var popUpMap = null;
 
 // mapbox Style layer name
 // style url: mapbox://styles/ciatph/cj67ldcoi0muu2smku4qksstd
 var mbLayerName = "amia-midres-tileset";
 
+// shapefile property name to hex color code key-pair
+var colorsNameMap = {
+    "Very Low": "#fff5f0",
+    "Low": "#fdbea5",
+    "Moderate": "#fc7050",
+    "High": "#d42020",
+    "Very High": "#67000d"   
+};
+
+
+/*
+* convert hex color code to RGBA
+* from https://stackoverflow.com/questions/21646738/convert-hex-to-rgba
+* @author ciatph
+*/
+function hexToRgbA(hex, opacity){
+    var fillOpacity = (opacity != null) ? opacity : 1.0;
+    var c;
+    if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
+        c= hex.substring(1).split('');
+        if(c.length== 3){
+            c= [c[0], c[0], c[1], c[1], c[2], c[2]];
+        }
+        c= '0x'+c.join('');
+        console.log("color: " + 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+','+ fillOpacity +')');
+        return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+','+ fillOpacity +')';
+    }
+    throw new Error('Bad Hex');
+}
 
 /*
  * generate a random RGBA color
@@ -27,15 +57,33 @@ var randomizeColors = function(){
 };
 
 
+/*
+* create a specified rgba color
+* @colorName RGB or hexcode color
+* @opacity color fill opacity (0.0 to 1.0)
+* @return RGBA output
+
+var createColor = function(colorName, opacity){
+    var transparency = (opacity != null) ? opacity : 0.8;
+    var color = (colorName.indexOf("rgba") >= 0) ? colorName : hexToRgbA(colorName, transparency);
+    var fillOpacity = (opacity != null) ? opacity : 1;
+}
+*/
+
+
 
 /*
  * create a random-colored layer with filter
  * @layerName custom mapbox layer ID
  * @filterValue data property name on which this colored layer is applied
+ * @colorValue html hexcode of specified color value. Takes a random color value if null
  * @author ciatph
  * @return json mapbox filtered layer
 */
-var createColoredLayer = function(layerName, filterValue){
+var createColoredLayer = function(layerName, filterValue, colorValue, opacity){
+    var colorLayer = (colorValue != null) ? colorValue : randomizeColors();
+    var transparency = (opacity != null) ? opacity : 0.8;
+
     // test: color all quezon green
     return {
         "id": layerName,
@@ -44,8 +92,8 @@ var createColoredLayer = function(layerName, filterValue){
         "source-layer": mbLayerName,
         "layout": {},
         "paint": {
-            "fill-color": randomizeColors(),
-            "fill-opacity": 0.8
+            "fill-color": colorLayer,
+            "fill-opacity": transparency
         },
         "filter": ["==", filterValue, ""]
     };   
@@ -55,6 +103,8 @@ var createColoredLayer = function(layerName, filterValue){
 /*
  * paint a random color overlay on top of each province
  * @propertyFilter name of GeoJSON/shapefile property on which to apply the color ("PROV_NAME")
+ *                 "PROV_NAME" : for municipalities
+                   "VA_Class" : for very high, low, moderate, high, very low classifications
  * @author ciatph
 */
 var colorLayersRandom = function(propertyFilter){
@@ -70,8 +120,7 @@ var colorLayersRandom = function(propertyFilter){
 
         if(provinces.indexOf(joined) == -1){
             provinces.push(joined);
-            console.log(joined);
-            map.addLayer(createColoredLayer("amia-" + joined, propertyFilter));
+            map.addLayer(createColoredLayer("amia-" + joined, propertyFilter, hexToRgbA(colorsNameMap[joined], 1.0)), 1.0);
             map.setFilter("amia-" + joined, ["==", propertyFilter, joined]); 
         }
     }   
