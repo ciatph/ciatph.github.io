423ebc497c095452ccd711887f0d574a5204ec51
madbarua <ciat.ph@gmail.com>
2018-09-04 08:04:35 +0800

    Toggle page header on continent click. Stored local js and css files. Uploaded from /terrain-gbif staging branch at revision 0d1ac55

--- terrai-gbif/js/legend.js
+++ terrai-gbif/js/legend.js
@@ -4,18 +4,13 @@ const maparea = { ASIA:1, LATIN_AMERICA:2 };
 
 // color list
 
-var colors = {
-    "base": "#fc8d59",
-    "p_andina_add": "#00ff00",
-    "p_salignus_add": "#2ca25f",
-    "p_uvi_add": "#3182bd"
-};
-
 var labels_colors = {
     "Terra-i Base deforestation detection": "#fc8d59",
     "Prumnopitys Andina": "#00ff00",
     "Pilgerodendron Uviferum": "#3182bd",
-    "Podocarpus Salignus": "#2ca25f"
+    "Podocarpus Salignus": "#2ca25f",
+    "Picea asperata": "#00ff00",
+    "Taiwania cryptomerioides": "#3182bd"  
 };
 
 
@@ -23,14 +18,14 @@ var labels_colors = {
  * Create a legend for LA
  * @param {Legend titles to print} cats 
  */
-var createLegend = function(){
+var createLegend = function(cats){
     var legend = L.control({position: 'bottomleft'});
 
     legend.onAdd = function (map) {
     var div = L.DomUtil.create('div', 'info-legend');
         labels = ['<strong>Categories</strong>'],
-        categories = ['Terra-i Base deforestation detection', 'Prumnopitys Andina', 'Pilgerodendron Uviferum', 'Podocarpus Salignus'];
-        // categories = cats;
+        //categories = ['Terra-i Base deforestation detection', 'Prumnopitys Andina', 'Pilgerodendron Uviferum', 'Podocarpus Salignus'];
+        categories = cats;
 
         for (var i = 0; i < categories.length; i++) {
             div.innerHTML += 
@@ -68,9 +63,11 @@ var toggleMap = function(maparea){
     console.log(maparea);
 
     if(maparea === 'asia'){
-        
+        removeLayers(mapLatin.layers);
+        loadMapAsia(basemap);
     }
     else if(maparea === 'la'){
-
+        removeLayers(mapAsia.layers);
+        loadMapLatin(basemap);
     }
 };
