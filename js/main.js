07d4e0a156175d99dac8e3baecceb2ac2fe15e53
ciatph <ciat.ph@gmail.com>
2019-06-21 04:18:59 +0800

    Deleted version_1.0.0 project files.

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){		  
-	$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
-		event.preventDefault(); 
-		event.stopPropagation(); 
-		$(this).parent().siblings().removeClass('open');
-		$(this).parent().toggleClass('open');
-	});		  
-});		
