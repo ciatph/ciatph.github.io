d1f0c7c121e801d54dab9acacc1f9269727d300f
ciatph <ciat.ph@gmail.com>
2019-08-05 11:05:40 +0800

    Updated contacts info. Fixed typos and broken links. Issue #4, Issue #11.

--- src/components/templates/Footer.vue
+++ src/components/templates/Footer.vue
@@ -18,28 +18,71 @@
     b-container(fluid style="padding-top:20px;")
       b-row(class="footer-style")
         b-col(sm="2")
-        b-col(sm="4" style="padding-right:40px;")
+        b-col(sm="3")
           // Partners Note
           h4 Partners
           p CGIAR-CCAFS is led by the International Center for Tropical Agriculture (CIAT).
-          p The Department of Agriculture (DA) is an official partner of theInternational Center for Tropical Agriculture (CIAT).
-        b-col(sm="2")
+          p The Department of Agriculture (DA) is an official partner of the International Center for Tropical Agriculture (CIAT).
+        b-col(sm="2" style="margin-left: 1%;")
           // Outputs Note
           h4 Output
-          ul
+          ul(style="list-style: none;")
             li
-              a(href="#") Documents
+              i(class="far fa-file-alt")
+              a(href="/#/crads") Decision Support Platform (CRA-DS)
             li
-              router-link(to="/maps" tag="a") CRVA Maps
+              router-link(to="/maps" tag="a") Climate Risk Vulnerability Assessment (CRVA)
+            li
+              a(href="/#/me") Monitoring and Evaluation (M&E)
             li
               a(href="http://cbatool.ciat.cgiar.org/") CBA Tool
-        b-col(sm="2" class="glyph-custom")
+
+        b-col(sm="3" class="glyph-custom")
           // Contact Note
           h4 Contact
-          ul
-            li(style="float:left;") Dindo M. Campilan
+
+          // Philippines Office
+          p(class="contacts")
+            span(class="contact-header") CIAT Philippine Country Office
             br
-            span D.Campilan@CGIAR.ORG
+            span c/o UPLB Foundation Inc., UPLB, College, Laguna 4030
+            br
+            span Contact: Mylene Aquino, Country Administrative Officer
+            br
+            span Phone: +63 49 5010581
+            br
+            span Email: mylene.aquino@cgiar.org
+
+          // Vietnam Office
+          p(class="contacts")
+            span(class="contact-header") CIAT Asia Regional Office
+            br
+            span c/o Agricultural Genetics Institute
+            br
+            span Pham Van Dong Street, Tu Liem, Hanoi, Vietnam
+            br
+            span Contact: Dr. Dindo M. Campilan, Director for Asia
+            br
+            span Phone: +84 4 37576969
+            br
+            span Fax: +84 4 37570999
+
+          // Dept. of Agriculture
+          p(class="contacts")
+            span(class="contact-header") Department of Agriculture – Systems-Wide Climate Change Office (DA-SWCCO)
+            br
+            span Adaptation and Mitigation Initiative in Agriculture (AMIA) Project
+            br
+            span Department of Agriculture Compound, Elliptical Road, Diliman
+            br
+            span Quezon City, PHILIPPINES 1104
+            br
+            span Contact: U-Nichols Manalo, Director
+            br
+            span Phone: +63 2 273-2489
+            br
+            span Email: climatechange.da@gmail.com
+
         b-col(sm="2")
     // Social Media Icons
     b-container(fluid)
