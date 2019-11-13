2031d21cc7a7dd1967d682ecc84b2ea19c88a4a1
ciatph <ciat.ph@gmail.com>
2019-11-13 17:23:36 +0800

    Updated crp filenames to match with latest updated pdf content.

--- src/defines/iconmaps/thumbnails-sub-crariskprofile.js
+++ src/defines/iconmaps/thumbnails-sub-crariskprofile.js
@@ -11,21 +11,21 @@ module.exports = {
       'title': '',
       'description': 'Luzon CRP',
       'img': 'th-crads-climaterisk-luzon.png',
-      'link': 'Luzon CRP.pdf',
+      'link': 'Luzon.pdf',
       'external': 'true'
     },
     {
       'title': '',
       'description': 'Visayas CRP',
       'img': 'th-crads-climaterisk-visayas.png',
-      'link': 'Visayas CRP.pdf',
+      'link': 'Visayas.pdf',
       'external': 'true'
     },
     {
       'title': '',
       'description': 'Mindanao CRP',
       'img': 'th-crads-climaterisk-mindanao.png',
-      'link': 'Mindanao CRP.pdf',
+      'link': 'Mindanao.pdf',
       'external': 'true'
     }
   ]
