fa8df86313274b9eea32b9a89dbcc5234df59f08
ciatph <ciat.ph@gmail.com>
2020-11-11 15:01:44 +0800

    Added website under maintenance page.

---
+++ src/components/templates/Maintenance.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  b-row(
+    class="text-center thumbnail-container")
+      b-col(sm="12")
+        h3 This page is currently down for maintenance.
+        br
+        br
+        br
+</template>
+
+<script>
+export default {
+  name: 'Maintenance'
+}
+</script>
---
+++ src/components/widgets/PageMaintenance.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  b-container(fluid class="container-fluid-custom bg-greylight")
+    maintenance
+
+</template>
+
+<script>
+import Maintenance from '@/components/templates/Maintenance'
+
+export default {
+  name: 'PageMaintenance',
+  components: {
+    Maintenance
+  }
+}
+</script>
